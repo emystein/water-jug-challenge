f30f9948b529aac5e51acbc8a55a566fe04ef567
Emiliano Menéndez <emystein@gmail.com>
2024-04-25 15:47:01 -0300

    Merged test cases.

--- __tests__/jugs.test.ts
+++ __tests__/jugs.test.ts
@@ -20,32 +20,16 @@ describe('Any of the Jugs has the same Capacity than the target Volume', () => {
 describe('One of the Jugs has less Capacity than the target Volume, the other Jug has sufficient Capacity to fill the target Volume', () => {
   it.each([
     [2, 1, 2],
+    [2, 1, 4],
     [4, 1, 4],
     [6, 2, 6],
-    [8, 2, 10],
-    [96, 2, 100],
-    [2, 2, 1],
-    [4, 4, 1],
-    [6, 6, 2],
-    [8, 10, 2],
-    [96, 100, 2]
-  ])
-  ('Expected: %p gal. Jug X capacity: %p gal. Jug Y capacity: %p gal', (expectedAmount: number, jugXCapacity: number, jugYCapacity: number) => {
-    const expectedGallons = new Gallons(expectedAmount);
-    const jugX = Jug.withCapacity(new Gallons(jugXCapacity));
-    const jugY = Jug.withCapacity(new Gallons(jugYCapacity));
-    const bartender = new Bartender(expectedGallons, jugX, jugY);
-    expect(bartender.mix()).toBeTruthy();
-  });
-});
-
-describe('One Jug has less capacity than the target Volume, the other Jug has more capacity than the target Volume', () => {
-  it.each([
-    [2, 1, 4],
     [6, 2, 8],
     [8, 2, 10],
     [96, 2, 100],
+    [2, 2, 1],
     [2, 4, 1],
+    [4, 4, 1],
+    [6, 6, 2],
     [6, 8, 2],
     [8, 10, 2],
     [96, 100, 2]
@@ -56,7 +40,7 @@ describe('One Jug has less capacity than the target Volume, the other Jug has mo
     const jugY = Jug.withCapacity(new Gallons(jugYCapacity));
     const bartender = new Bartender(expectedGallons, jugX, jugY);
     expect(bartender.mix()).toBeTruthy();
-  })
+  });
 });
 
 describe('No solution', () => {
