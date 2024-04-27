3908baca9d68ebc5641fd05f31ea3bde196ddcbe
Emiliano Menéndez <emystein@gmail.com>
2024-04-26 21:48:22 -0300

    Minor index cleanup.

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { mix } from './controllers/MixController';
 import bodyParser from 'body-parser';
 
 export const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.set("json spaces", 2);
 
 app.post('/mix', mix);
 
+const PORT = process.env.PORT || 3000;
+
 export const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
