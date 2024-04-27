4c5b9a6cbee29a9fea35163f323dd1ce948d6443
Emiliano Menéndez <emystein@gmail.com>
2024-04-27 09:34:46 -0300

    Implement the strategy pattern for different mix recipes.

--- src/model/mixer.ts
+++ src/model/mixer.ts
@@ -5,62 +5,101 @@ import { EmptyJug, FillJug, TransferContentBetweenJugs } from './mixActions';
 import Jug from './jug';
 
 export default class Mixer {
+  private recipes: MixRecipe[];
+
   constructor(public targetVolume: PositiveGallons, private logger: MixLogger) {
+    this.recipes = [
+      new FillOnlyOneJug(this),
+      new TransferFromSmallerToBiggerJug(this),
+      new TransferFromBiggerToSmallerJug(this)
+    ]
   }
 
   mix(jugs: Jugs): MixResult {
-    if (jugs.haveAJugWithTheSameCapacityThan(this.targetVolume)) {
-      this.fulfillFillingAnyJug(jugs);
-      return MixResult.Solved;
-    }
-
-    if (jugs.jugWithNearestCapacityTo(this.targetVolume) == jugs.smallerJug) {
-      this.fulfillTransferringFromSmallerToBiggerJug(jugs);
+    const recipe = this.recipes.find(aRecipe => aRecipe.appliesTo(jugs, this.targetVolume));
+    if (recipe) {
+      return recipe.mix(jugs, this.targetVolume);
     } else {
-      this.fulfillTransferringFromBiggerToSmallerJug(jugs);
-    }
-
-    const isSolved = jugs.haveAJugFilledWithVolume(this.targetVolume);
-    return isSolved ? MixResult.Solved : MixResult.NoSolution;
-  }
-
-  private fulfillFillingAnyJug(jugs: Jugs) {
-    const jugsWithSameCapacityThanTargetVolume = jugs.allWithTheSameCapacityThan(this.targetVolume);
-    if (jugsWithSameCapacityThanTargetVolume.length > 0) {
-      this.fill(jugsWithSameCapacityThanTargetVolume[0]);
+      return MixResult.NoSolution;
     }
   }
 
-  private fulfillTransferringFromBiggerToSmallerJug(jugs: Jugs) {
-    this.fill(jugs.biggerJug);
-    while (jugs.biggerJug.volumeFilled.isHigherThan(this.targetVolume)) {
-      this.empty(jugs.smallerJug);
-      this.transferBetween(jugs.biggerJug, jugs.smallerJug, jugs.smallerJug.capacity);
-    }
-  }
-
-  private fulfillTransferringFromSmallerToBiggerJug(jugs: Jugs) {
-    let accumulatedVolume = new Gallons(0);
-    while (accumulatedVolume.isLowerThan(this.targetVolume)) {
-      this.fill(jugs.smallerJug);
-      accumulatedVolume = accumulatedVolume.plus(jugs.smallerJug.capacity);
-      this.transferBetween(jugs.smallerJug, jugs.biggerJug, accumulatedVolume);
-    }
-  }
-
-  private fill(aJug: Jug): void {
+  fill(aJug: Jug): void {
     aJug.fill();
     this.logger.addEntry(new FillJug(aJug));
   }
 
-  private empty(aJug: Jug): void {
+  public empty(aJug: Jug): void {
     aJug.empty();
     this.logger.addEntry(new EmptyJug(aJug));
   }
 
-  private transferBetween(sender: Jug, receiver: Jug, accumulatedVolume: Gallons): void {
+  public transferBetween(sender: Jug, receiver: Jug, accumulatedVolume: Gallons): void {
     sender.transferContentTo(receiver);
     this.logger.addEntry(new TransferContentBetweenJugs(sender, receiver, accumulatedVolume));
+  }
+}
+
+abstract class MixRecipe {
+  constructor(protected mixer: Mixer) {
+
+  }
+
+  abstract appliesTo(jugs: Jugs, targetVolume: Gallons): boolean;
+
+  mix(jugs: Jugs, targetVolume: Gallons): MixResult {
+    this.prepare(jugs, targetVolume);
+    return this.checkResult(jugs, targetVolume);
+  }
+
+  abstract prepare(jugs: Jugs, targetVolume: Gallons): void;
+
+  private checkResult(jugs: Jugs, targetVolume: Gallons) {
+    if (jugs.haveAJugFilledWithVolume(targetVolume)) {
+      return MixResult.Solved;
+    } else {
+      return MixResult.NoSolution;
+    }
+  }
+}
+
+class FillOnlyOneJug extends MixRecipe {
+  appliesTo(jugs: Jugs, targetVolume: Gallons): boolean {
+    return jugs.haveAJugWithTheSameCapacityThan(targetVolume);
+  }
+
+  prepare(jugs: Jugs, targetVolume: Gallons): void {
+    const firstMatchingJug = jugs.allWithTheSameCapacityThan(targetVolume)[0];
+    this.mixer.fill(firstMatchingJug);
+  }
+}
+
+class TransferFromSmallerToBiggerJug extends MixRecipe {
+  appliesTo(jugs: Jugs, targetVolume: Gallons): boolean {
+    return jugs.jugWithNearestCapacityTo(targetVolume) == jugs.smallerJug;
+  }
+
+  prepare(jugs: Jugs, targetVolume: Gallons): void {
+    let accumulatedVolume = new Gallons(0);
+    while (accumulatedVolume.isLowerThan(targetVolume)) {
+      this.mixer.fill(jugs.smallerJug);
+      accumulatedVolume = accumulatedVolume.plus(jugs.smallerJug.capacity);
+      this.mixer.transferBetween(jugs.smallerJug, jugs.biggerJug, accumulatedVolume);
+    }
+  }
+}
+
+class TransferFromBiggerToSmallerJug extends MixRecipe {
+  appliesTo(jugs: Jugs, targetVolume: Gallons): boolean {
+    return jugs.jugWithNearestCapacityTo(targetVolume) == jugs.biggerJug;
+  }
+
+  prepare(jugs: Jugs, targetVolume: Gallons): void {
+    this.mixer.fill(jugs.biggerJug);
+    while (jugs.biggerJug.volumeFilled.isHigherThan(targetVolume)) {
+      this.mixer.empty(jugs.smallerJug);
+      this.mixer.transferBetween(jugs.biggerJug, jugs.smallerJug, jugs.smallerJug.capacity);
+    }
   }
 }
 
